d414abf7c72fc31eac08fc98cd820a0e23927c1d
meliomano <elioperez.conde@gmail.com>
2022-10-27 12:32:37 +0200

    Done until move_semantics2.rs

--- exercises/primitive_types/primitive_types3.rs
+++ exercises/primitive_types/primitive_types3.rs
@@ -2,11 +2,11 @@
 // Create an array with at least 100 elements in it where the ??? is.
 // Execute `rustlings hint primitive_types3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 fn main() {
-    let a = ???
+    //let a = ["Are we there yet?"; 10];
+    let a = "A".repeat(100);
 
+    println!("a = {:?}\n", a);
     if a.len() >= 100 {
         println!("Wow, that's a big array!");
     } else {
